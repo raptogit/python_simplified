5aed34a2845663cf7900ea1df52e6e3fabd08495
raptoservices <57556559+raptoservices@users.noreply.github.com>
2020-10-06 16:35:34 +0530

    cont

---
+++ Programs/dataTypes.py
@@ -0,0 +1,17 @@
+#data Types
+
+
+a=50                                #integer data type
+print(a)
+
+str='''Rapto Learning '''           #String data type
+print(str)
+
+flot=32.2                           #Float data type
+print(flot)
+
+x=1j                                #Complex data type
+print(x)
+
+b=False                             #Boolean data type
+print(b)
---
+++ Programs/variables.py
@@ -0,0 +1,4 @@
+a= "Rapto Learning"
+b= 150
+c=20.9
+print(a,b,c)
